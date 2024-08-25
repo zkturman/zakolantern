026d1a9741f94f9ff929e23cbff2d2e0a765fe72
Zack Turman <zkturman@gmail.com>
2024-08-25 17:03:57 +0100

    updating to create a single update loop for game

--- src/app/movie-game/movie-game.component.ts
+++ src/app/movie-game/movie-game.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as PIXI from 'pixi.js';
 
 @Component({
@@ -6,14 +6,16 @@ import * as PIXI from 'pixi.js';
   templateUrl: './movie-game.component.html',
   styleUrls: ['./movie-game.component.css']
 })
-export class MovieGameComponent implements OnInit, AfterViewInit {
+export class MovieGameComponent implements OnInit {
 
   private app: PIXI.Application;
   private skullImage: PIXI.Texture;
   private enemies: PIXI.Sprite[] = [];
+  private enemyData: any[] = [];
   private playerImage: PIXI.Texture;
   private player: PIXI.Sprite;
   private playerDirection: number = 0;
+  private laserShots: PIXI.Sprite[] = [];
 
   
   constructor() { 
@@ -41,9 +43,15 @@ export class MovieGameComponent implements OnInit, AfterViewInit {
     let targetWidth = this.app.screen.width / targetNumber;
     for (let i = 0; i < targetNumber; i++){
       let newSprite = this.createTarget(targetWidth * i + (targetWidth / 2), this.app.screen.height / 2);
-      this.enemies.push(newSprite);
     }
     this.createPlayer();
+    this.app.ticker.add((time) => this.gameUpdate(time));
+  }
+
+  gameUpdate(delta){
+    this.playerUpdate(delta);
+    this.laserUpdate(delta);
+    this.enemyUpdate(delta);
   }
 
   createTarget(xPosition, yPosition): PIXI.Sprite{
@@ -53,39 +61,52 @@ export class MovieGameComponent implements OnInit, AfterViewInit {
     skull.anchor.set(0.5);
     skull.x = xPosition;
     skull.y = yPosition;
-
-    this.app.stage.addChild(skull);
-    let originalX = skull.x;
-    let maxMovement = 10;
-    let currentDirection = 1;
-    this.app.ticker.add((time) =>{
-      let currentRelativeX = skull.x - originalX;
-      if (currentDirection === 1 && currentRelativeX > maxMovement){
-        currentDirection = -1;
-      }
-      if (currentDirection === -1 && currentRelativeX < (maxMovement * -1)){
-        currentDirection = 1;
-      }
-      skull.x += 0.1 * time.deltaTime * currentDirection * 3;
+    let direction = Math.round(Math.random()) === 1 ? 1 : -1;
+    this.enemies.push(skull);
+    this.enemyData.push({
+      "x": skull.x, 
+      "y": skull.y, 
+      "direction": direction
     });
+    this.app.stage.addChild(skull);
     return skull;
+  }
+
+  enemyUpdate(time){
+    for (let i = 0; i < this.enemies.length; i++){
+      let enemy = this.enemies[i];
+      let originalX = this.enemyData[i].x;
+      let maxMovement = 10;
+      let currentDirection = this.enemyData[i].direction;
+      let currentRelativeX = enemy.x - originalX;
+      if (currentDirection === 1 && currentRelativeX > maxMovement){
+        this.enemyData[i].direction = Math.round(Math.random()) - 1;
+      }
+      else if (currentDirection === -1 && currentRelativeX < (maxMovement * -1)){
+        this.enemyData[i].direction = Math.round(Math.random());
+      }
+      else if (currentDirection === 0){
+        this.enemyData[i].direction = Math.round((Math.random() * 2) - 1);
+      }
+      enemy.x += 0.1 * time.deltaTime * currentDirection * 3;
+    }
   }
 
   createPlayer(){
     this.player = new PIXI.Sprite(this.skullImage);
-    this.player.height = 50;
-    this.player.width = 50;
+    this.player.height = this.app.screen.width * 0.05;
+    this.player.width = this.app.screen.width * 0.05;
     this.player.anchor.set(0.5);
     this.player.x = this.app.screen.width / 2;
     this.player.y = this.app.screen.height - 50;
-
     this.app.stage.addChild(this.player);
-    this.app.ticker.add((time) => {
-      if (!this.playerWithinBound()){
-        this.playerDirection = 0;
-      }
-      this.player.x += 0.1 * time.deltaTime * this.playerDirection * 5;
-    });
+  }
+
+  playerUpdate(time){
+    if (!this.playerWithinBound()){
+      this.playerDirection = 0;
+    }
+    this.player.x += 0.1 * time.deltaTime * this.playerDirection * 15;
   }
 
   movePlayer(value){
@@ -111,10 +132,19 @@ export class MovieGameComponent implements OnInit, AfterViewInit {
 
 
   fireLaser(){
-    console.log('laser fired!');
+    let laserBar = new PIXI.Sprite(this.skullImage);
+    laserBar.height = this.app.screen.width * 0.05;
+    laserBar.width = this.app.screen.width * 0.01;
+    laserBar.anchor.set(0.5);
+    laserBar.x = this.player.x;
+    laserBar.y = this.player.y - (this.player.height / 2);
+    this.app.stage.addChild(laserBar);
+    this.laserShots.push(laserBar);
   }
 
-  ngAfterViewInit(): void {
+  laserUpdate(time){
+    for (let i = this.laserShots.length - 1; i >= 0; i--){
+      this.laserShots[i].y -= 0.1 * 20;
+    }
   }
-
 }
